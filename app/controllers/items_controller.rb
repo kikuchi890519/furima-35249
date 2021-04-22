3440911f514be371e2c4a5bff511f1358ec71158
kikuchi890519 <kikuchi.890519@gmail.com>
2021-04-22 13:41:37 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
   # ログインユーザーのみにアクセスを許可する。
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.all
@@ -22,16 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -45,4 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :details, :category_id, :condition_id, :shipping_charge_id, :city_id, :days_ship_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
