8d334df3e034319fb37679c973af8bd4722bf403
kikuchi890519 <kikuchi.890519@gmail.com>
2021-04-22 12:37:40 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインユーザーのみにアクセスを許可する。
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.all
@@ -25,22 +25,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-    # unless @item.user_id == current_user.id
-    #   redirect_to root_path
-    # end
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
 
-  # def updated
-  #   @item = Item.find(params[:id])
-  #   if @item.update(item_params)
-  #      redirect_to item_path
-
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
 
